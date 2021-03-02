7b705f53c06bcada47804d445032b3d91e1468a9
sunny <sunnychoudhury007@gmail.com>
2021-03-02 13:20:13 +0530

    Added API based architecture

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -2,11 +2,11 @@ import React, { useEffect } from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 
 const App = () => {
-  useEffect(() => {
-    /*eslint no-undef: "off"*/
-    initializeLogger();
-    logger.info("Log from js-logger");
-  }, []);
+  // useEffect(() => {
+  //   /*eslint no-undef: "off"*/
+  //   initializeLogger();
+  //   logger.info("Log from js-logger");
+  // }, []);
 
   return (
     <Router>
